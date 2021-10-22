3db0a0054c7e5aeda9e5a672254d71b020d80cef
kaviarasanR <91777015+kaviarasanR@users.noreply.github.com>
2021-10-22 21:14:11 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+package week1.day2;
+
+public class Calculator {
+
+	public void addition() {
+		int num1 = 5;
+		int num2 = 15;
+		int num3 = 30;
+		int add = num1 + num2 + num3;
+		System.out.println(add);
+	}
+
+	public void subraction() {
+		int num1 = 10;
+		int num2 = 2;
+		int sub = num1 - num2;
+		System.out.println(sub);
+	}
+
+	public void multiplication() {
+		double num1 = 8.22;
+		double num2 = 9.57;
+		double mul = num1 * num2;
+		System.out.println(mul);
+	}
+
+	public void division() {
+		float num1 = 9.8f;
+		float num2 = 3.4f;
+		float div = num1 / num2;
+		System.out.println(div);
+
+	}
+
+}
+
---
+++ Mycalculator.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Mycalculator {
+
+	public static void main(String[] args) {
+		Calculator operate = new Calculator();
+		operate.addition();
+		operate.subraction();
+		operate.division();
+		operate.multiplication();
+
+	}
+
+}
